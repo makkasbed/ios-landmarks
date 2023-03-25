6e3989d15e6cdb98865c6b5b9c60e8e8fa80f6aa
adu <aluta182004@gmail.com>
2023-03-25 21:57:59 +0000

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by adu bediako asare on 25/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
