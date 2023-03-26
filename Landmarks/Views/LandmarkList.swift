13137cee4592c96af40f53af7688396e1e739177
adu <aluta182004@gmail.com>
2023-03-26 11:51:45 +0000

    implemented observables

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -8,25 +8,37 @@
 import SwiftUI
 
 struct LandmarkList: View {
-    var body: some View {
-        NavigationView{
-            List(landmarks) { landmark in
-                NavigationLink {
-                                   LandmarkDetail(landmark: landmark)
-                               } label: {
-                                   LandmarkRow(landmark: landmark)
-                            }
+    @EnvironmentObject var modelData: ModelData
+    
+    @State private var showFavoritesOnly = false
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+                (!showFavoritesOnly || landmark.isFavorite)
             }
-        }.navigationTitle("Landmarks")
+        }
+    
+    var body: some View {
+        NavigationView {
+                   List {
+                       Toggle(isOn: $showFavoritesOnly) {
+                                           Text("Favorites only")
+                       }
+                       ForEach(filteredLandmarks) { landmark in
+                           NavigationLink {
+                               LandmarkDetail(landmark: landmark)
+                           } label: {
+                               LandmarkRow(landmark: landmark)
+                           }
+                       }
+                   }
+                   .navigationTitle("Landmarks")
+        }
     }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
+    
     static var previews: some View {
-        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
-                    LandmarkList()
-                        .previewDevice(PreviewDevice(rawValue: deviceName))
-                        .previewDisplayName(deviceName)
-                }
+        LandmarkList()
     }
 }
--- Landmarks/Views/LandmarkRow.swift
+++ Landmarks/Views/LandmarkRow.swift
@@ -14,13 +14,18 @@ struct LandmarkRow: View {
         HStack{
             landmark.image.resizable()
                 .frame(width: 50, height: 50)
-            Spacer()
             Text(landmark.name)
+            Spacer()
+            if landmark.isFavorite {
+                            Image(systemName: "star.fill")
+                    .foregroundColor(.yellow)
+            }
         }
     }
 }
 
 struct LandmarkRow_Previews: PreviewProvider {
+    static var landmarks = ModelData().landmarks
     static var previews: some View {
         Group{
             LandmarkRow(landmark: landmarks[0])
